37375ea3978ad086c444674d27ebfe227196bd9a
usernameIsJam <jammm093@gmail.com>
2021-11-24 22:39:02 +0900

    scheduler - 2021.11.24

---
+++ src/scheduler/MyObject.java
@@ -0,0 +1,18 @@
+package scheduler;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class MyObject implements Job{
+
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		
+		System.out.println("Hurry !!! MyScheuler is running!!!");
+		System.out.println("The time now is " + new Date());
+	}
+
+}
